22bec90c2558f7addde0477d696bbc275390718d
matvey-zgursky <matveyzgursky@gmail.com>
2026-05-29 22:17:18 +0700

    раздокументирование скрапинга любимых треков

--- main.py
+++ main.py
@@ -25,15 +25,15 @@ def main() -> None:
 
         ui.show_selected_playlist(target_playlist)
 
-        # liked_tracks = LikedTracks(spotify)
-        # track_uris = liked_tracks.get_uris_by_added_year()
-        # if not track_uris:
-        #     print(f"No liked tracks found for {ADDED_YEAR}.")
-        #     return
+        liked_tracks = LikedTracks(spotify)
+        track_uris = liked_tracks.get_uris_by_added_year()
+        if not track_uris:
+            print(f"No liked tracks found for {ADDED_YEAR}.")
+            return
 
-        # print(f"Liked track URIs added in {ADDED_YEAR}:")
-        # for index, track_uri in enumerate(track_uris, start=1):
-        #     print(f"{index}. {track_uri}")
+        print(f"Liked track URIs added in {ADDED_YEAR}:")
+        for index, track_uri in enumerate(track_uris, start=1):
+            print(f"{index}. {track_uri}")
     except Exception as error:
         print(f"Program failed: {error}")
         raise SystemExit(1)
